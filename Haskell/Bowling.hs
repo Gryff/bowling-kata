a383276df40bdfd587218b78d7d8c0b804f53352
Gryff <gryffskin@gmail.com>
2016-05-13 10:49:21 +0100

    scoreGame returns 90

---
+++ Haskell/Bowling.hs
@@ -0,0 +1,4 @@
+module Bowling where 
+
+scoreGame :: String -> Int 
+scoreGame game = 90
--- Haskell/BowlingTests.hs
+++ Haskell/BowlingTests.hs
@@ -6,3 +6,11 @@ import Test.Hspec
 import Test.QuickCheck
 import Text.Printf (printf)
 
+testScoreGame :: String -> Int -> Spec
+testScoreGame game score =
+    it (printf "should return the score for game : %s --> %d \n" game score) $
+        scoreGame game `shouldBe` score
+
+main = hspec $ do 
+    describe "scoreGame" $ do 
+        testScoreGame "9-9-9-9-9-9-9-9-9-9-" 90
