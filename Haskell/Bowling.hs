ef092d3f1127f13c8f505238098945597a0ad87b
Gryff <gryffskin@gmail.com>
2016-05-17 10:54:21 +0100

    Cover edge cases, remove unnecessary brackets

--- Haskell/Bowling.hs
+++ Haskell/Bowling.hs
@@ -2,10 +2,71 @@ module Bowling where
 
 import Data.Char
 
-scoreGame :: String -> Int 
-scoreGame game = foldl addIfInt 0 game
+data Frame = Strike | Spare Char | Simple Char Char
+    deriving (Eq, Show)
 
-addIfInt :: Int -> Char -> Int
-addIfInt total bowl
-    | isDigit bowl == True = total + digitToInt bowl
-    | otherwise = total 
+type FrameNumber = Int
+
+
+scoreGame :: String -> Int 
+scoreGame game = addFrames $ zip [1..] $ parse game
+
+parse :: String -> [Frame]
+parse [] = []
+parse (x:[])
+    | x == 'X' = [Strike]
+    | otherwise = [Simple x '-']
+parse (x:y:z:[])
+    | x == 'X' = [Strike] ++ parse [y] ++ parse [z]
+    | otherwise = parse [x,y] ++ parse [z]
+parse (x:y:frames)
+    | x == 'X' = Strike : parse(y:frames)
+    | y == '/' = Spare x : parse frames
+    | otherwise = Simple x y : parse frames
+
+addFrames :: [(FrameNumber, Frame)] -> Int
+addFrames [] = 0
+addFrames (frame:[]) = frameValue frame       
+addFrames (frame1:frame2:[]) = 
+    case frame1 of 
+        (10,Spare x) -> 10 + bonusPoints "Spare" [frame2]
+        (_,Spare x) -> 10 + (bonusPoints "Spare" [frame2]) + addFrames [frame2]
+        (_,Simple x y) -> frameValue frame1 + addFrames [frame2]
+addFrames (frame1:frame2:frame3:[]) = 
+    case frame1 of 
+        (10, Strike) -> 10 + bonusPoints "Strike" [frame2,frame3]
+        (_, Strike) -> 10 + (bonusPoints "Strike" [frame2,frame3]) + addFrames [frame2,frame3]
+        (_,Spare _) -> 10 + (bonusPoints "Spare" [frame2]) + addFrames [frame2,frame3]
+        (_,Simple _ _) -> frameValue frame1 + addFrames [frame2,frame3]
+addFrames (frame1:frame2:frame3:frames) = 
+    case frame1 of
+        (_,Strike) -> 10 + (bonusPoints "Strike" [frame2,frame3]) + addFrames(frame2:frame3:frames)
+        (_,Spare _) -> 10 + (bonusPoints "Spare" [frame2]) + addFrames(frame2:frame3:frames)
+        (_,Simple _ _) -> frameValue frame1 + addFrames(frame2:frame3:frames)
+
+frameValue :: (FrameNumber,Frame) -> Int
+frameValue (_, Strike) = 10
+frameValue (_, Spare x) = rollValue x
+frameValue (_, Simple x y) = rollValue x + rollValue y
+
+rollValue :: Char -> Int 
+rollValue x = if x == '-' then 0 else digitToInt x
+
+bonusPoints :: String -> [(FrameNumber,Frame)] -> Int
+bonusPoints bowlType frames =
+    case bowlType of 
+        "Strike" -> getTwoRolls frames 
+        "Spare" -> getOneRoll $ frames !! 0
+
+getTwoRolls :: [(FrameNumber,Frame)] -> Int 
+getTwoRolls (frame1:frame2:[]) = 
+    case frame1 of 
+        (_,Strike) -> 10 + getOneRoll frame2
+        (_,Spare _) -> 10
+        (11,Simple _ _) -> getOneRoll frame1 + getOneRoll frame2
+        (_,Simple _ _) -> frameValue frame1
+
+getOneRoll :: (FrameNumber,Frame) -> Int 
+getOneRoll (_,Strike) = 10
+getOneRoll (n,Spare x) = frameValue (n,Spare x)
+getOneRoll (_,Simple x _) = rollValue x
--- Haskell/BowlingTests.hs
+++ Haskell/BowlingTests.hs
@@ -10,9 +10,26 @@ testScoreGame :: String -> Int -> Spec
 testScoreGame game score =
     it (printf "should return the score for game : %s --> %d \n" game score) $
         scoreGame game `shouldBe` score
+        
+testParse :: String -> [Frame] -> Spec
+testParse game frames =
+    it (printf "should return frames : %s \n" game) $
+        parse game `shouldBe` frames
 
 main = hspec $ do 
     describe "scoreGame" $ do 
+        testScoreGame"--------------------" 0
+        testScoreGame "1-1----------------1" 3
         testScoreGame "9-9-9-9-9-9-9-9-9-9-" 90
         testScoreGame "1-1-1-1-1-1-1-1-1-1-" 10
         testScoreGame "12131415161718171611" 59
+        testScoreGame "5/5/5/5/5/5/5/5/5/5/5" 150
+        testScoreGame "XXXXXXXXXXXX" 300
+        testScoreGame "XXXXXXXXXX12" 274
+        testScoreGame "1/35XXX458/X3/23" 160
+        testScoreGame "1/35XXX458/X3/XX6" 189
+        testScoreGame "5/11------------3/11" 26
+    describe "parse" $ do 
+        testParse "9-9-9-9-9-9-9-9-9-9-" (replicate 10 (Simple '9' '-'))
+        testParse "5/5/5/5/5/5/5/5/5/5/5" (replicate 10 (Spare '5') ++ [Simple '5' '-'])
+        testParse "1/35XXX458/X3/23" [Spare '1', Simple '3' '5', Strike, Strike, Strike, Simple '4' '5', Spare '8', Strike, Spare '3', Simple '2' '3']
